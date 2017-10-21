f4f4ec100a7b2cc581f1bdf31609eb7c361ba139
PointerFLY <linghao.michael.ma@gmail.com>
2017-10-21 16:06:44 +0100

    Add main.py for test

---
+++ search/main.py
@@ -0,0 +1,26 @@
+import pacman
+from pacman import readCommand, runGames
+
+argvStrList = []
+for _ in range(20):
+    argvStrList.append("")
+argvStrList[0] = "-l mediumMaze -p SearchAgent --frameTime 0 -a fn=dfs"
+argvStrList[1] = "-l mediumMaze -p SearchAgent --frameTime 0 -a fn=bfs"
+argvStrList[2] = "-l mediumMaze -p SearchAgent --frameTime 0 -a fn=ucs"
+argvStrList[3] = "-l mediumMaze -p SearchAgent --frameTime 0 -a fn=astar"
+argvStrList[4] = "-l mediumCorners -p SearchAgent -a fn=bfs,prob=CornersProblem --frameTime 0"
+argvStrList[5] = "-l mediumCorners -p SearchAgent -a fn=aStarSearch,prob=CornersProblem,heuristic=cornersHeuristic -z 0.5 --frameTime 0"
+argvStrList[6] = "-l trickySearch -p SearchAgent -a fn=astar,prob=FoodSearchProblem,heuristic=foodHeuristic --frameTime 0"
+argvStrList[7] = "-l bigSearch -p ClosestDotSearchAgent -z 0.5 --frameTime 0"
+
+for idx, argvStr in enumerate(argvStrList):
+    if idx > 0:
+        break
+    print "********", idx, "*********"
+    argvList = argvStr.split(" ")
+    args = readCommand(argvList)
+    runGames(**args)
+
+# argvList = argvStrList[4].split(" ")
+# args = readCommand(argvList)  # Get game components based on input
+# runGames(**args)
