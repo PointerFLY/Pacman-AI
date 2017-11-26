bf1a438609b10e4399772b1eae9ddd85ca308a3c
PointerFLY <linghao.michael.ma@gmail.com>
2017-11-26 16:00:51 +0000

    q6 passed

--- reinforcement/analysis.py
+++ reinforcement/analysis.py
@@ -60,9 +60,7 @@ def question3e():
     # If not possible, return 'NOT POSSIBLE'
 
 def question6():
-    answerEpsilon = None
-    answerLearningRate = None
-    return answerEpsilon, answerLearningRate
+    return 'NOT POSSIBLE'
     # If not possible, return 'NOT POSSIBLE'
 
 if __name__ == '__main__':
